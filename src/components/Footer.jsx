4f725081a0cd291fb36e1509b54dcb8e0583b755
Vasiliy <75576725+milan9111@users.noreply.github.com>
2022-09-17 18:07:17 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = (props) => {
         <footer className="footer">
             <div className="footer__text">
                 <div className="footer__monolit">
-                    <time dateTime="2021">2021</time> © MONOLIT GYM.
+                    <time dateTime="2022">2022</time> © MONOLIT GYM.
                 </div>
                 <div className="footer__creator">
                     <a href="https://www.linkedin.com/in/vasiliy-zakrghevskyi-5527041b8/" target="_blank">Site created: Vasiliy Zakrghevskyi <i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
