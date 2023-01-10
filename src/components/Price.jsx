8f5191edbfef76022e9ad54a151b4f24ad16c335
milan9111 <milan9111@ukr.net>
2023-01-10 12:44:46 +0200

    added discount

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,43 +1,46 @@
 import React from "react";
-import './../styles/Price.css';
-
-
+import "./../styles/Price.css";
 
 const Price = (props) => {
-    return(
-        <section className="price" id="price-scroll">
-            <div className="price__box">
-                <div className="price__title">
-                    <h2>
-                        Послуги
-                    </h2>
-                </div>
-                <div className="price__services">
-                    <div className="price__item">
-                        <span>Безлімітне відвідування</span>
-                        <span className="price__item_price">- 600 грн/місяць</span>  
-                    </div>
-                    <div className="price__item">
-                        <span>Ранковий безліміт</span>
-                        <span className="price__item_price">- 550 грн/місяць</span>  
-                    </div>
-                    <div className="price__item">
-                        <span>Шкільний безліміт</span>
-                        <span className="price__item_price">- 350 грн/місяць</span>  
-                    </div>
-                    <div className="price__item">
-                        <span>Студентський безліміт</span>
-                        <span className="price__item_price">- 480 грн/місяць</span>
-                         
-                    </div>
-                    <div className="price__item">
-                        <span>8 тренувань за 30 днів</span>
-                        <span className="price__item_price">- 480 грн/місяць</span>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="price" id="price-scroll">
+      <div className="price__box">
+        <div className="price__title">
+          <h2>Послуги</h2>
+        </div>
+        <div className="price__services">
+          <div className="price__item">
+            <span>Річний абонемент</span>
+            <span className="price__item_price">- 4000 грн</span>
+            {/* remove after 01.02.2023 */}
+            <span className="price__item_price" style={{ color: "red" }}>
+              *Акція до 01.02.2023! Для жінок - 3500 грн
+            </span>
+          </div>
+          <div className="price__item">
+            <span>Безлімітне відвідування</span>
+            <span className="price__item_price">- 600 грн/місяць</span>
+          </div>
+          <div className="price__item">
+            <span>Ранковий безліміт</span>
+            <span className="price__item_price">- 550 грн/місяць</span>
+          </div>
+          <div className="price__item">
+            <span>Шкільний безліміт</span>
+            <span className="price__item_price">- 350 грн/місяць</span>
+          </div>
+          <div className="price__item">
+            <span>Студентський безліміт</span>
+            <span className="price__item_price">- 480 грн/місяць</span>
+          </div>
+          <div className="price__item">
+            <span>8 тренувань за 30 днів</span>
+            <span className="price__item_price">- 480 грн/місяць</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Price;
