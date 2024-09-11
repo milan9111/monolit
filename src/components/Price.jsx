60253ae66b1f097c64fe398d1d4bd0414ce907c2
Vasyl <75576725+milan9111@users.noreply.github.com>
2024-09-11 18:02:01 +0300

    Update Price.jsx

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -11,27 +11,27 @@ const Price = (props) => {
         <div className="price__services">
           <div className="price__item">
             <span>Річний абонемент</span>
-            <span className="price__item_price">- 5000 грн</span>
+            <span className="price__item_price">- 6000 грн</span>
           </div>
           <div className="price__item">
             <span>Безлімітне відвідування</span>
-            <span className="price__item_price">- 700 грн/місяць</span>
+            <span className="price__item_price">- 800 грн/місяць</span>
           </div>
           <div className="price__item">
             <span>Ранковий безліміт</span>
-            <span className="price__item_price">- 650 грн/місяць</span>
+            <span className="price__item_price">- 700 грн/місяць</span>
           </div>
           <div className="price__item">
             <span>Шкільний безліміт</span>
-            <span className="price__item_price">- 400 грн/місяць</span>
+            <span className="price__item_price">- 500 грн/місяць</span>
           </div>
           <div className="price__item">
             <span>Студентський безліміт</span>
-            <span className="price__item_price">- 600 грн/місяць</span>
+            <span className="price__item_price">- 650 грн/місяць</span>
           </div>
           <div className="price__item">
             <span>8 тренувань за 30 днів</span>
-            <span className="price__item_price">- 600 грн/місяць</span>
+            <span className="price__item_price">- 700 грн/місяць</span>
           </div>
         </div>
       </div>
